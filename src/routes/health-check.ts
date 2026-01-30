b569b1fd79920b44f144e30e602784ddd87791ab
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2026-01-30 19:44:26 -0300

    new: configure deploy task ECS

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -5,6 +5,6 @@ const MAXIMUM_FILE_SIZE_IN_BYTES = 1024 * 1024 * 4 // 4mb
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
 
-    await reply.status(201).send({ message: "Tudo OK!" })
+    await reply.status(201).send({ message: "Tudo OK agora no ECS!" })
   })
 }
